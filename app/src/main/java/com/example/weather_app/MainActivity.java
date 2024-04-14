00a431f848c5638c1725a7c3356028947fe58a4b
kevin-ada <kevinkoech265@gmail.com>
2024-04-14 17:24:30 +0300

    Detailed Weather App

---
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.weather_app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    String city = "Nairobi";
+    String API = "f0ef868cc17fb73d890b79daa5d778a9";
+
+
+    TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt, sunriseTxt,
+            sunsetTxt, windTxt, pressureTxt, humidityTxt;
+
+    SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        addressTxt = findViewById(R.id.address);
+        updated_atTxt = findViewById(R.id.updated_at);
+        statusTxt = findViewById(R.id.status);
+        tempTxt = findViewById(R.id.temp);
+        temp_minTxt = findViewById(R.id.temp_min);
+        temp_maxTxt = findViewById(R.id.temp_max);
+        sunriseTxt = findViewById(R.id.sunrise);
+        sunsetTxt = findViewById(R.id.sunset);
+        windTxt = findViewById(R.id.wind);
+        pressureTxt = findViewById(R.id.pressure);
+        humidityTxt = findViewById(R.id.humidity);
+        searchView = findViewById(R.id.searchView);
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                city = query;
+                addressTxt.setText(city);
+                new weatherTask().execute();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+        new weatherTask().execute();
+    }
+
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            /* Showing the ProgressBar, Making the main design GONE */
+            findViewById(R.id.loader).setVisibility(View.VISIBLE);
+            findViewById(R.id.mainContainer).setVisibility(View.GONE);
+            findViewById(R.id.errorText).setVisibility(View.GONE);
+
+}
+
+        protected String doInBackground(String... args) {
+            String response = RequestHttp.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + API);
+            return response;
+        }
+
+        @Override
+    protected void onPostExecute(String result) {
+        try {
+            JSONObject jsonObj = new JSONObject(result);
+            JSONObject main = jsonObj.getJSONObject("main");
+            JSONObject sys = jsonObj.getJSONObject("sys");
+            JSONObject wind = jsonObj.getJSONObject("wind");
+            JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+            Long updatedAt = jsonObj.getLong("dt");
+            String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+            String temp = main.getString("temp") + "°C";
+            String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+            String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+            String pressure = main.getString("pressure");
+            String humidity = main.getString("humidity");
+
+            Long sunrise = sys.getLong("sunrise");
+            Long sunset = sys.getLong("sunset");
+            String windSpeed = wind.getString("speed");
+            String weatherDescription = weather.getString("description");
+
+            String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+            /* Populating extracted data into our views */
+            addressTxt.setText(address);
+            updated_atTxt.setText(updatedAtText);
+            statusTxt.setText(weatherDescription.toUpperCase());
+            tempTxt.setText(temp);
+            temp_minTxt.setText(tempMin);
+            temp_maxTxt.setText(tempMax);
+            sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
+            sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
+            windTxt.setText(windSpeed);
+            pressureTxt.setText(pressure);
+            humidityTxt.setText(humidity);
+
+            /* Views populated, Hiding the loader, Showing the main design */
+            findViewById(R.id.loader).setVisibility(View.GONE);
+            findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            findViewById(R.id.loader).setVisibility(View.GONE);
+            findViewById(R.id.errorText).setVisibility(View.VISIBLE);
+        }
+
+        }
+    }
+}
+
